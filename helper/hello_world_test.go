66bba5fc9ea4a0a9a6add4df45d6764eddbaadf9
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2023-02-20 13:52:39 +0700

    update

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,15 +1,50 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"runtime"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestMain(m *testing.M) {
+	//before
+	fmt.Println("BEFORE UNIT TEST")
+
+	m.Run() //mengeksekusi semua unit test
+
+	//after
+	fmt.Println("AFTER UNIT TEST")
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Cannot run on Windows OS ")
+	}
+	result := HelloWorld("Marlin")
+	require.Equal(t, "Hello Marlin", result, "Result must be Hello Marlin")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Marlin")
+	require.Equal(t, "Hello Marlin", result, "Result must be Hello Marlin") //require jika gagal memanggil FailNow()
+	fmt.Println("Hello World with Require Done")
+}
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Marlin")
+	assert.Equal(t, "Hello Marlin", result, "Result must be Hello Marlin") //assert jika gagal memanggil Fail()
+	fmt.Println("Hello World with Assert Done")
+}
+func TestHelloWorldMarlin(t *testing.T) {
 	result := HelloWorld("Marlin")
 
 	if result != "Hello Marlin" {
 		//error
-		panic("Result is not 'Hello Marlin'")
-
+		// t.Fail() //tetap mengeksekusi kode dibawahnya
+		t.Error("Result must be 'Hello Marlin'")
 	}
+	fmt.Println("TestHelloWorldMarlin Done")
 }
 
 func TestHelloWorldPurnama(t *testing.T) {
@@ -17,7 +52,8 @@ func TestHelloWorldPurnama(t *testing.T) {
 
 	if result != "Hello Purnama" {
 		//error
-		panic("Result is not 'Hello Purnama'")
-
+		// t.FailNow() //jika fail, kode dibawahnya tidak lagi dieksekusi langsung berhenti
+		t.Fatal("Result must be 'Hello Purnama'")
 	}
+	fmt.Println("TestHelloWorldPurnama Done")
 }
