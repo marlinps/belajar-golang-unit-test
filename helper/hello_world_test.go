135ee428e6ef7b20b4f437bb574148c2da5b8745
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2023-02-20 16:48:13 +0700

    add table test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -8,7 +8,61 @@ import (
 	"testing"
 )
 
-func TestMain(m *testing.M) {
+func TestTableHelloWorld(t *testing.T) { //Table Test, lebih efisien dibandingkan metode testing-testing sebelumnya
+	tests := []struct { //slice struct
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Marlin",
+			request:  "Marlin",
+			expected: "Hello Marlin",
+		},
+		{
+			name:     "Purnama",
+			request:  "Purnama",
+			expected: "Hello Purnama",
+		},
+		{
+			name:     "Sari",
+			request:  "Sari",
+			expected: "Hello Sari",
+		},
+		{
+			name:     "Ririn",
+			request:  "Ririn",
+			expected: "Hello Ririn",
+		},
+		{
+			name:     "Rayyan",
+			request:  "Rayyan",
+			expected: "Hello Rayyan",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			require.Equal(t, test.expected, result)
+		})
+	}
+}
+
+func TestSubTest(t *testing.T) { //SubTest
+	t.Run("Marlin", func(t *testing.T) {
+		result := HelloWorld("Marlin")
+		require.Equal(t, "Hello Marlin", result, "Result mus be 'Hello Marlin'")
+	})
+	t.Run("Purnama", func(t *testing.T) {
+		result := HelloWorld("Purnama")
+		require.Equal(t, "Hello Purnama", result, "Result mus be 'Hello Purnama'")
+	})
+
+	//untuk menjalankan test di subtest tertentu perintah contoh go test -run TestSubTest/Marlin
+}
+
+func TestMain(m *testing.M) { //Before and After Test
 	//before
 	fmt.Println("BEFORE UNIT TEST")
 
@@ -18,7 +72,7 @@ func TestMain(m *testing.M) {
 	fmt.Println("AFTER UNIT TEST")
 }
 
-func TestSkip(t *testing.T) {
+func TestSkip(t *testing.T) { //TestSkip
 	if runtime.GOOS == "windows" {
 		t.Skip("Cannot run on Windows OS ")
 	}
